43a95603bf7bb8a39c07d2c5b5377948409dcbf7
ThamanGurung01 <tgurung519@gmail.com>
2025-03-21 07:44:49 +0545

    profile route

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,13 +1,14 @@
 "use client"
 import React from 'react'
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 const Navbar = () => {
   return (
     <div className="bg-white">
         <div className='w-full flex justify-between px-10 py-6 border-b-2 border-gray-300'>
         <h1 className='text-3xl font-bold'>Blood Bank Management</h1>
         <div>
-        <img src="/blood.png" className='rounded-full w-10 h-10 border-2' alt="Profile" />
+        <Link href={"/profile"}><img src="/blood.png" className='rounded-full w-10 h-10 border-2' alt="Profile" /></Link>
         </div>
         </div>
     </div>
