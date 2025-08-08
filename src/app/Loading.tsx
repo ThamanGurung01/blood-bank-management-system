8082ed781f1879362c8eb49ca8b6e6dd03e7ab28
ThamanGurung01 <tgurung519@gmail.com>
2025-08-08 07:14:15 +0545

    loading change

--- src/app/Loading.tsx
+++ src/app/Loading.tsx
@@ -1,11 +1,12 @@
-import {TailSpin } from 'react-loader-spinner';
-
 const Loading = () => {
-  return (
-    <div className="flex justify-center items-center h-screen w-full">
-      <TailSpin height="60" width="60" color="#4f46e5" />
-    </div>
-  );
-};
+    return (
+      <div className="flex h-[70vh] items-center justify-center w-full">
+        <div className="flex flex-col items-center">
+          <div className="h-12 w-12 animate-spin rounded-full border-4 border-red-600 border-t-transparent"></div>
+          <p className="mt-4 text-gray-600">Loading...</p>
+        </div>
+      </div>
+    );
+}
 
-export default Loading;
+export default Loading
